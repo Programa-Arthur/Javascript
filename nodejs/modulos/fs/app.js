b066726dab7b87a1afb4b78921fc22fb94d33008
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-09-02 09:26:51 -0300

    versao modulos fs os

---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('exemplo.txt', 'utf8', (erro, conteudo_do_arquivo) => {
+    if (erro){
+        console.error('erro ao ler o arquivo', erro);
+        return;
+    }
+    console.log('conteudo do arquivo:', conteudo_do_arquivo);
+});
+
+console.log('esta mensagem aparece primeiro');
